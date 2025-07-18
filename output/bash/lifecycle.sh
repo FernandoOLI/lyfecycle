4a54104733f924cead8a8e5ed53f771920fd9952
fernando <oliveira-fernando1@hotmail.com>
2025-07-17 23:38:47 -0300

    test

--- output/bash/lifecycle.sh
+++ output/bash/lifecycle.sh
@@ -5,4 +5,4 @@ echo Applying lifecycle to bucket: lyfecycle-management-bucket
 
 aws s3api put-bucket-lifecycle-configuration \
 --bucket lyfecycle-management-bucket \
---lifecycle-configuration file://../rules/raw_lifecycle.json
+--lifecycle-configuration file://../rules/data_lifecycle.json
